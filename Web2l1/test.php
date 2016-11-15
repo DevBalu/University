735cc0f8c400a2427ffc9373bd8bc0dd106dd24c
DevBalu <ghenov.andrei@gmail.com>
2016-11-15 12:35:07 +0200

    Finish Web2l1

--- Web2l1/test.php
+++ Web2l1/test.php
@@ -3,10 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
 </head>
 <body>
 
 	<form action="rezultate.php" method="POST">
+		<div class="input-field col s12">
+		    <select single>
+		      <option value="" disabled selected>Choose your option</option>
+		      <option value="1">Option 1</option>
+		      <option value="2">Option 2</option>
+		      <option value="3">Option 3</option>
+		    </select>
+		    <label>Materialize Multiple Select</label>
+		  </div>
+
 		<h2>Fill in the fields correctly</h2>
 		<h3>Capital of  Great Britan</h3>
 		<select name="cGB">
@@ -34,6 +45,7 @@
 
 		<button type="submit">button</button>
 	</form>
-	
+	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 </body>
 </html>
